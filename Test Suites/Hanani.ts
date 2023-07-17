5463c55189bfc5b5962d797703b99bee1c963c5a
Hanani Faqihah <Hanani Faqihah@Hanani-Faqihah.realtek>
2023-07-17 14:56:07 +0800

    tetsing

---
+++ Test Suites/Hanani.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Hanani</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ae49240d-4803-4512-b350-1ae94788ca18</testSuiteGuid>
+   <testCaseLink>
+      <guid>3e94cc5b-b049-42ad-956d-a9a611c9c5f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login,Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
